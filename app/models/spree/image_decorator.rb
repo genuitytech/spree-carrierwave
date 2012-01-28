ac3d1f7326f79cf8611f8482f7ed0f6cfadeceb3
hoverlover <hoverlover@gmail.com>
2012-01-27 21:18:23 -0600

    Decorators have to be named *_decorator.rb.  Duh!

---
+++ app/models/spree/image_decorator.rb
@@ -0,0 +1,41 @@
+Spree::Image.class_eval do
+  class Uploader < CarrierWave::Uploader::Base
+    include CarrierWave::Compatibility::Paperclip
+    include CarrierWave::MiniMagick
+
+    storage :fog
+
+    # Spree looks in attachment#errors, so just delegate to model#errors
+    delegate :errors, :to => :model
+
+    # Match the path defined in Spree::Image
+    def paperclip_path
+      "assets/products/:id/:style/:basename.:extension"
+    end
+
+    # These are the versions defined in Spree::Image
+    version :mini do
+      process :resize_to_fill => [48, 48]
+    end
+
+    version :small do
+      process :resize_to_fill => [100, 100]
+    end
+
+    version :product do
+      process :resize_to_fill => [240, 240]
+    end
+
+    version :large do
+      process :resize_to_fill => [600, 600]
+    end
+  end
+
+  mount_uploader :attachment, Uploader, :mount_on => :attachment_file_name
+
+  # Get rid of the paperclip callbacks
+
+  def save_attached_files; end
+  def prepare_for_destroy; end
+  def destroy_attached_files; end
+end
